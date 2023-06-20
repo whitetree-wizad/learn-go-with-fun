1820ebc6ccf1721d753f2967fcc77160c4e67f7e
whitetree-wizad <chi.chu.wm@outlook.com>
2023-06-20 21:23:34 +0800

    Changes rover.go

--- 长时间运行的工作进程/rover.go
+++ 长时间运行的工作进程/rover.go
@@ -10,7 +10,7 @@ type command int
 
 const (
 	right = command(0)
-	left = command(1)
+	left  = command(1)
 )
 
 type RoverDriver struct {
@@ -18,38 +18,54 @@ type RoverDriver struct {
 }
 
 func NewRoverDriver() *RoverDriver {
-	r:=&RoverDriver{
+	r := &RoverDriver{
 		commandc: make(chan command),
 	}
 	go r.drive()
 	return r
 }
 
+func (r *RoverDriver) Left() {
+	r.commandc <- left
+}
+
+func (r *RoverDriver) Right() {
+	r.commandc <- right
+}
+
 func (r *RoverDriver) drive() {
-	pos:=image.Point{X:0,Y:0}
-	direction:=image.Point{X: 1,Y: 0}
-	updateInterval:=250*time.Millisecond
-	nextMove:=time.After(updateInterval)
-	for  {
+	pos := image.Point{X: 0, Y: 0}
+	direction := image.Point{X: 1, Y: 0}
+	updateInterval := 250 * time.Millisecond
+	nextMove := time.After(updateInterval)
+	for {
 		select {
-		case c:=<-r.commandc:
+		case c := <-r.commandc:
 			switch c {
 			case right:
 				direction = image.Point{
-				X: -direction.Y,
-				Y: direction.X,
+					X: -direction.Y,
+					Y: direction.X,
 				}
 			case left:
-				direction=image.Point{
-				X: direction.Y,
-				Y: -direction.X,
+				direction = image.Point{
+					X: direction.Y,
+					Y: -direction.X,
 				}
 			}
-			log.Printf("new direction %v",direction)
+			log.Printf("new direction %v", direction)
 		case <-nextMove:
 			pos = pos.Add(direction)
-			log.Printf("move to %v",pos)
-			nextMove=time.After(updateInterval)
+			log.Printf("move to %v", pos)
+			nextMove = time.After(updateInterval)
 		}
 	}
+}
+
+func main() {
+	r := NewRoverDriver()
+	time.Sleep(3 * time.Second)
+	r.Right()
+	time.Sleep(3 * time.Second)
+	r.Left()
 }
