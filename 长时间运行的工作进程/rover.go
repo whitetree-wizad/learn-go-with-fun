d37fda1292e1d35e4f35928132dfb8145baeb87a
“whitetree-wizad” <chi.chu.wm@outlook.com>
2023-06-20 22:40:28 +0800

    Changes rover.go

--- 长时间运行的工作进程/rover.go
+++ 长时间运行的工作进程/rover.go
@@ -11,6 +11,8 @@ type command int
 const (
 	right = command(0)
 	left  = command(1)
+	start = command(2)
+	stop  = command(3)
 )
 
 type RoverDriver struct {
@@ -33,9 +35,18 @@ func (r *RoverDriver) Right() {
 	r.commandc <- right
 }
 
+func (r *RoverDriver) Start() {
+	r.commandc <- start
+}
+
+func (r *RoverDriver) Stop() {
+	r.commandc <- stop
+}
+
 func (r *RoverDriver) drive() {
 	pos := image.Point{X: 0, Y: 0}
 	direction := image.Point{X: 1, Y: 0}
+	var aspect command
 	updateInterval := 250 * time.Millisecond
 	nextMove := time.After(updateInterval)
 	for {
@@ -43,14 +54,37 @@ func (r *RoverDriver) drive() {
 		case c := <-r.commandc:
 			switch c {
 			case right:
+				aspect = right
 				direction = image.Point{
 					X: -direction.Y,
 					Y: direction.X,
 				}
 			case left:
+				aspect = left
 				direction = image.Point{
 					X: direction.Y,
 					Y: -direction.X,
+				}
+			case stop:
+				log.Printf("rover stop at %v", pos)
+				direction = image.Point{
+					X: 0,
+					Y: 0,
+				}
+			case start:
+				log.Printf("rover begin to move")
+				direction = image.Point{X: 1, Y: 0}
+				switch aspect {
+				case right:
+					direction = image.Point{
+						X: -direction.Y,
+						Y: direction.X,
+					}
+				case left:
+					direction = image.Point{
+						X: direction.Y,
+						Y: -direction.X,
+					}
 				}
 			}
 			log.Printf("new direction %v", direction)
@@ -66,6 +100,10 @@ func main() {
 	r := NewRoverDriver()
 	time.Sleep(3 * time.Second)
 	r.Right()
+	time.Sleep(3 * time.Second)
+	r.Stop()
+	time.Sleep(3 * time.Second)
+	r.Start()
 	time.Sleep(3 * time.Second)
 	r.Left()
 }
